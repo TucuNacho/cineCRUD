a2a64837f556cadb6df469d7e854d7ecbb35c134
Nacho <barrojojoaquin2@gmail.com>
2025-06-06 21:16:55 -0300

    agregamos funcion para ver los detalles de la pelicula

--- JS/classeCine.js
+++ JS/classeCine.js
@@ -1,72 +1,81 @@
 export default class crearPelicula {
-    #id
-    #titulo
-    #director
-    #anio
-    #genero
-    #duracion
-    constructor (titulo, director, anio, genero, duracion){
-        this.#id = crypto.randomUUID(); 
-        this.#titulo = titulo
-        this.#director = director
-        this.#anio = anio
-        this.#genero = genero
-        this.#duracion = duracion
-    }
-    // get de peliculas
-    get id() {
-        return this.#id;
-    }
+  #id;
+  #titulo;
+  #director;
+  #anio;
+  #genero;
+  #imagen;
+  #duracion;
+  constructor(titulo, director, anio, genero, duracion, imagen) {
+    this.#id = crypto.randomUUID();
+    this.#titulo = titulo;
+    this.#director = director;
+    this.#anio = anio;
+    this.#genero = genero;
+    this.#duracion = duracion;
+    this.#imagen = imagen;
+  }
+  // get de peliculas
+  get id() {
+    return this.#id;
+  }
 
+  get titulo() {
+    return this.#titulo;
+  }
 
-    get titulo() {
-        return this.#titulo
-    }
+  get director() {
+    return this.#director;
+  }
 
-    get director() {
-        return this.#director
-    }
+  get anio() {
+    return this.#anio;
+  }
 
-    get anio() {
-        return this.#anio
-    }
+  get genero() {
+    return this.#genero;
+  }
 
-    get genero() {
-        return this.#genero
-    }
+  get duracion() {
+    return this.#duracion;
+  }
 
-    get duracion(){
-        return this.#duracion
-    }
-    //set de peliculas
-    set titulo(nuevoTitulo){
-        this.#titulo = nuevoTitulo
-    }
+  get imagen() {
+    return this.#imagen;
+  }
+  //set de peliculas
+  set titulo(nuevoTitulo) {
+    this.#titulo = nuevoTitulo;
+  }
 
-    set director(nuevoDirector) {
-        this.#director= nuevoDirector
-    }
+  set director(nuevoDirector) {
+    this.#director = nuevoDirector;
+  }
 
-    set anio(nuevoAnio) {
-        this.#anio = nuevoAnio
-    }
+  set anio(nuevoAnio) {
+    this.#anio = nuevoAnio;
+  }
 
-    set genero(nuevoGenero) {
-        this.#genero= nuevoGenero
-    }
+  set genero(nuevoGenero) {
+    this.#genero = nuevoGenero;
+  }
 
-    set duracion(nuevaDuracion){
-        this.#duracion = nuevaDuracion
-    }
+  set duracion(nuevaDuracion) {
+    this.#duracion = nuevaDuracion;
+  }
+  set imagen(nuevaImagen) {
+    this.#imagen = nuevaImagen;
+  }
 
-    toJSON() {
-        return{
-            id: this.id,
-            titulo: this.titulo,
-            director: this.#director,
-            anio: this.anio,
-            genero: this.genero,
-            duracion: this.duracion
-        }
-    }
+  toJSON() {
+    return {
+      id: this.id,
+      titulo: this.titulo,
+      director: this.#director,
+      anio: this.anio,
+      genero: this.genero,
+      duracion: this.duracion,
+      imagen: this.imagen,
+    };
+  }
 }
